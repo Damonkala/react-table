4c4e22dfaf2d5995d40064400c0082b5066e2f3e
Damon Kalaboukis <damonkala@yahoo.com>
2015-12-04 13:45:35 -0800

    Googlelogo

--- public/script.js
+++ public/script.js
@@ -2,15 +2,18 @@
 
 var React = require("react");
 var ReactDOM = require("react-dom");
+var GoogleLogo = require("./GoogleLogo.js")
+
 
 var H1C = React.createClass({
 	render: function(){
-		return React.createElement("h1", {className: "green"},
-			"Changed Text");
+		return React.createElement("div", {className: "green"},
+			"Hello world", React.createElement(GoogleLogo));
 	}
 });
 
 
 
 
+// ReactDOM.render(React.createElement(GoogleLogo), document.getElementById('react'))
 ReactDOM.render(React.createElement(H1C), document.getElementById('react'))
