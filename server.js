89971c60b66a8a1f1c19fe09067fb342c7ea7c16
Damon Kalaboukis <damonkala@yahoo.com>
2015-12-04 11:42:30 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var PORT = process.env.PORT || 3000;
+
+var express = require('express');
+
+
+var app = express();
+
+
+app.use(express.static('public'));
+
+
+app.listen(PORT, function(){
+  console.log('Listening on port ', PORT);
+});
